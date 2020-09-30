b1160f183107059d4b6b62cca3ebdebb20a2702a
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-09-29 21:59:49 -0700

    changed app.js script to use arrow functions instead

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,18 +8,31 @@ var tbody = d3.select("tbody");
 // console.log(tableData);
 
 // Loop through the data to get an object for each ufo data record
-tableData.forEach(function(ufo) {
-    console.log(ufo);
+// tableData.forEach(function(ufo) {
+//     console.log(ufo);
+//     var row = tbody.append("tr");
+
+//     Object.entries(ufo).forEach(function([key, value]) {
+//         console.log(key,value);
+
+//         // Append 1 cell for each ufo value (date, city, state, country, shape, duration, and comment )
+//         var cell = tbody.append("td");
+
+//         // Add the ufo data to the table
+//         cell.text(value);
+//     });
+    
+// });
+
+tableData.forEach((ufo) => {
     var row = tbody.append("tr");
 
-    Object.entries(ufo).forEach(function([key, value]) {
-        console.log(key,value);
+    Object.entries(ufo).forEach(([key, value]) => {
 
         // Append 1 cell for each ufo value (date, city, state, country, shape, duration, and comment )
         var cell = tbody.append("td");
 
         // Add the ufo data to the table
         cell.text(value);
-    });
-    
+    }); 
 });
