73a87a624f28a614fb33ab27225487b6c2c5abaa
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-09-29 22:50:58 -0700

    got the click part of the button working and the value captured

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,26 +4,7 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
-// console.log(tableData);
-
 // Loop through the data to get an object for each ufo data record
-// tableData.forEach(function(ufo) {
-//     console.log(ufo);
-//     var row = tbody.append("tr");
-
-//     Object.entries(ufo).forEach(function([key, value]) {
-//         console.log(key,value);
-
-//         // Append 1 cell for each ufo value (date, city, state, country, shape, duration, and comment )
-//         var cell = tbody.append("td");
-
-//         // Add the ufo data to the table
-//         cell.text(value);
-//     });
-    
-// });
-
 tableData.forEach((ufo) => {
     var row = tbody.append("tr");
 
@@ -36,3 +17,55 @@ tableData.forEach((ufo) => {
         cell.text(value);
     }); 
 });
+
+// Select the Filter UFO Table button
+// Select the form
+let button = d3.select("#filter-btn"),
+    form = d3.select("#form");
+
+// Complete the event handler function for the form
+const runEnter = () => {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get teh raw HTML node
+    let inputElement = d3.select("#datetime-input");
+
+    // Get the value property of the input element
+    let inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+
+
+
+}
+
+
+
+// // The click handler for the form
+// filterufo.on("click", function() {
+
+//     // Prevent the page from refreshing
+//     d3.event.preventDefault();
+
+//     // Select the input element and get the raw HTML node
+//     var inputElement = d3.select("datetime-input"); 
+
+//     // Get the value property of the input element
+//     var inputValue = inputElement.property("value");
+
+//     console.log(inputValue);
+//     console.log(tableData);
+
+
+
+
+// });
+
+
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
