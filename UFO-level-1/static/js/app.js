4cc0c975701cd7881a02954caa1afd2e7b078c03
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-09-29 21:38:24 -0700

    started working on app.js file and added d3 reference in index.html

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,19 @@
 // from data.js
 var tableData = data;
 
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
 // YOUR CODE HERE!
+// console.log(tableData);
+
+// Loop through the data to get an object for each ufo data record
+tableData.forEach(function(ufo) {
+    console.log(ufo);
+    var row = tbody.append("tr");
+
+    Object.entries(ufo).forEach(function([key, value]) {
+        console.log(key,value);
+    })
+    
+});
