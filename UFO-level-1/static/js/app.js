e9840d74f8a39534ff30a17559381a46873130a2
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-10-01 21:42:25 -0700

    fixed table so td tags are inside tr tags

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,10 +11,11 @@ tableData.forEach((ufo) => {
     Object.entries(ufo).forEach(([key, value]) => {
 
         // Append 1 cell for each ufo value (date, city, state, country, shape, duration, and comment )
-        var cell = tbody.append("td");
+        var cell = row.append("td");
 
         // Add the ufo data to the table
         cell.text(value);
+        console.log(cell.text(value));
 
     }); 
 });
@@ -43,20 +44,42 @@ const runEnter = () => {
 
     console.log(filteredData);
 
-    var tr = d3.select("tbody")
-        .selectAll("tr")  // selects all <tr>
-        .data(d3.keys(filteredData))      // joins filteredData array 
-        .enter()           // create placeholders for each row in the array
-        .append("tr");// create <tr> in each placeholder
+    var table, tr, td, i, txtValue;
+    table = document.getElementById("ufo-table")[0];
+    console.log(table);
 
-    var td = tr.selectAll("td")
-        .data(function (d) {return d})    // joins inner array of each row
-       // console.log(d.value);
-        .enter()    // create placeholders for each element in an inner array
-        .append("td") // creates <td> in each placeholder
-        .text(function (d) {return d});
-        // console.log(d);
-        // return d; // add value of each inner array as a text in <td>
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those that don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.indexOf(inputValue) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+
+
+
+    // var tr = d3.select("tbody")
+    //     .selectAll("tr")  // selects all <tr>
+    //     .data(d3.keys(filteredData))      // joins filteredData array 
+    //     .enter()           // create placeholders for each row in the array
+    //     .append("tr");// create <tr> in each placeholder
+
+    // var td = tr.selectAll("td")
+    //     .data(function (d) {return d})    // joins inner array of each row
+    //    // console.log(d.value);
+    //     .enter()    // create placeholders for each element in an inner array
+    //     .append("td") // creates <td> in each placeholder
+    //     .text(function (d) {return d});
+    //     // console.log(d);
+    //     // return d; // add value of each inner array as a text in <td>
 
 }
 
