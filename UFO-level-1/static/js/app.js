14cd6aa779fdffacfd318bbec2f5826b37d14adc
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-10-03 16:00:21 -0700

    cleaned up app.js file and style.css, level 1 in done

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -35,13 +35,8 @@ const runEnter = () => {
     // Get the value property of the input element
     let inputValue = inputElement.property("value");
 
-    console.log(inputValue);
-    console.log(tableData);
-
     // Use the form input to filter the data by date
     let filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-
-    console.log(filteredData);
 
     // Clear out the table rows
     tbody.html("");
@@ -60,69 +55,7 @@ const runEnter = () => {
 
         }); 
     });
-
-    // var table, tr, td, i, txtValue;
-    // table = document.getElementById("ufo-table");
-    // //console.log(table);
-
-    // tr = table.getElementsByTagName("tr");
-
-    // // Loop through all table rows, and hide those that don't match the search query
-    // for (i = 0; i < tr.length; i++) {
-    //     td = tr[i].getElementsByTagName("td")[0];
-    //     if (td) {
-    //         txtValue = td.textContent || td.innerText;
-    //         if (txtValue.indexOf(inputValue) > -1) {
-    //             tr[i].style.display = "";
-    //         } else {
-    //             tr[i].style.display = "none";
-    //         }
-    //     }
-    // }
-
-
-
-
-    // var tr = d3.select("tbody")
-    //     .selectAll("tr")  // selects all <tr>
-    //     .data(d3.keys(filteredData))      // joins filteredData array 
-    //     .enter()           // create placeholders for each row in the array
-    //     .append("tr");// create <tr> in each placeholder
-
-    // var td = tr.selectAll("td")
-    //     .data(function (d) {return d})    // joins inner array of each row
-    //    // console.log(d.value);
-    //     .enter()    // create placeholders for each element in an inner array
-    //     .append("td") // creates <td> in each placeholder
-    //     .text(function (d) {return d});
-    //     // console.log(d);
-    //     // return d; // add value of each inner array as a text in <td>
-
-}
-
-
-
-// // The click handler for the form
-// filterufo.on("click", function() {
-
-//     // Prevent the page from refreshing
-//     d3.event.preventDefault();
-
-//     // Select the input element and get the raw HTML node
-//     var inputElement = d3.select("datetime-input"); 
-
-//     // Get the value property of the input element
-//     var inputValue = inputElement.property("value");
-
-//     console.log(inputValue);
-//     console.log(tableData);
-
-
-
-
-// });
-
-
+};
 
 // Create event handlers 
 button.on("click", runEnter);
